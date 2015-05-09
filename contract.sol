5839ded3033f6393bbe43b65bc87e755bd815002
ethers <ethereum@outlook.com>
2015-05-08 18:23:53 -0700

    createTicket()

--- contract.sol
+++ contract.sol
@@ -18,11 +18,14 @@ contract EthereumBitcoinSwap {
 		ClaimData claimData;
 	}
 	
+	// try saving with uint64 ?
 	uint private gTicketId = 1;  // start out at 1 to minimize issues where an uninitialized variable leads to a valid ticket
 	
-	mapping (uint => Ticket) tickets;
+	mapping (uint => Ticket) gTicket;
 	
-	function createTicket() {
-		
+	function createTicket(address btcAddr, bytes10 numWei, bytes2 weiPerSatoshi) returns (uint) {
+		gTicket[gTicketId].ticketData.btcAddr = btcAddr;
+		gTicket[gTicketId].ticketData.numWei = numWei;
+		gTicket[gTicketId].ticketData.weiPerSatoshi = weiPerSatoshi;
 	}
 }
