3ede1be00b1a60ce05d0ebe2c3287c5e91ce64f5
ethers <ethereum@outlook.com>
2015-05-19 22:46:10 -0700

    fix offer page; need to fix table

--- frontend/js/shared.js
+++ frontend/js/shared.js
@@ -1,9 +1,10 @@
 var WEI_PER_SATOSHI = new BigNumber(10).pow(10);
+var SATOSHI_PER_BTC = new BigNumber(10).pow(8);
 
 function formatEtherAmount(nWei) {
   return web3.fromWei(nWei, 'ether');
 }
 
 function formatUnitPrice(nWeiPerSatoshi) {
-  return new BigNumber(nWeiPerSatoshi).div(WEI_PER_SATOSHI);
+  return SATOSHI_PER_BTC.div(nWeiPerSatoshi);
 }
