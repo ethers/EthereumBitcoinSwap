421a6331f5c7ed35e0e4c43f90ca3ae6eeeb6905
ethers <ethereum@outlook.com>
2015-05-25 18:10:15 -0700

    should not have closing input tag

--- meteor-dapp/client/templates/elements/offerEther.js
+++ meteor-dapp/client/templates/elements/offerEther.js
@@ -1,7 +1,11 @@
 Template.offerEther.viewmodel({
-  btcAddr: '',
-  numEther: '',
-  btcPrice: '',
+  btcAddr: '1Ed53ZSJiL5hF9qLonNPQ6CAckKYsNeWwJ',
+  numEther: '5.2',
+  btcPrice: '0.26',
+
+  // btcAddr: '',
+  // numEther: '',
+  // btcPrice: '',
 
   unitPrice: function() {
     var bnEther = new BigNumber(this.numEther());
