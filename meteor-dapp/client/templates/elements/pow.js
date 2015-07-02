58360ec9bbf203ae832c724fb22bf384642dd8b3
ethers <ethereum@outlook.com>
2015-07-02 15:25:32 -0700

    fix Pow #29

--- meteor-dapp/client/templates/elements/pow.js
+++ meteor-dapp/client/templates/elements/pow.js
@@ -6,12 +6,12 @@ var kecc = new ku.Keccak();
 Template.pow.viewmodel({
   btcTxHash: 'dd5a8f13c97c8b8d47329fa7bd487df24b7d3b7e855a65eb7fd51e8f94f7e482',
   ticketId: 2,
-  nonce: 1007349,
+  nonce: 2460830,
 
   findPoWClicked: function() {
     console.log(this.btcTxHash())
 
-    var hexTicketId = this.ticketId().toString(16);
+    var hexTicketId = new BigNumber(this.ticketId()).toString(16);
     var padLen = 16 - hexTicketId.length;
     var leadZerosForTicketId = Array(padLen + 1).join('0');
 
@@ -61,11 +61,11 @@ Template.pow.viewmodel({
   },
 
   verifyPoWClicked: function() {
-    var hexTicketId = this.ticketId().toString(16);
+    var hexTicketId = new BigNumber(this.ticketId()).toString(16);
     var padLen = 16 - hexTicketId.length;
     var leadZerosForTicketId = Array(padLen + 1).join('0');
 
-    var hexNonce = this.nonce().toString(16);
+    var hexNonce = new BigNumber(this.nonce()).toString(16);
     padLen = 16 - hexNonce.length;
     var leadZerosForNonce = Array(padLen + 1).join('0');
 
@@ -73,6 +73,8 @@ Template.pow.viewmodel({
     var src;
     var bnHash;
     var strHash;
+
+    console.log('@@@ bnSrc: ', bnSrc.toString(16))
 
 
     src = ku.hexStringToBytes(bnSrc.toString(16));
@@ -85,7 +87,7 @@ Template.pow.viewmodel({
     bnHash = new BigNumber('0x' + strHash);
 
     var isPowValid = bnHash.lt(bnTarget);
-    console.log('@@@ isPowValid: ', isPowValid)
+    console.log('@@@ isPowValid: ', isPowValid, ' pow: ', bnHash.toString(16), ' target: ', bnTarget.toString(16))
 
     if (isPowValid) {
       swal('Proof of Work', 'Valid', 'success');
