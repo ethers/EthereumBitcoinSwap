e3ae685e319523612731ddc7645f6ab51afd48fa
ethers <ethereum@outlook.com>
2015-07-09 11:46:22 -0700

    deploy on Olympic (RC2)

--- meteor-dapp/client/lib/init.js
+++ meteor-dapp/client/lib/init.js
@@ -1,4 +1,4 @@
-web3.setProvider(new web3.providers.HttpProvider('http://localhost:8999'));
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 
 web3.eth.defaultAccount = web3.eth.coinbase;
 
@@ -16,8 +16,9 @@ WEI_PER_SATOSHI = new BigNumber(10).pow(10);
 
 useBtcTestnet = true;
 if (useBtcTestnet) {
-  gTicketContractAddr = '0xc53a82b9b7c9af4801c7d8ea531719e7657aff3c';  // private
+  // gTicketContractAddr = '0xc53a82b9b7c9af4801c7d8ea531719e7657aff3c';  // private
   // gTicketContractAddr = '0x8901a2bbf639bfd21a97004ba4d7ae2bd00b8da8';  // reserveFastExpiry
+  gTicketContractAddr = '0x39dfd4315e8b90488ab57e58e4d8b4597a1511e6';  // Olympic
   // gTicketContractAddr = '0xb007e8d073af6b6487261bc06660f87ea8740230';
   gVersionAddr = 111;
   //
