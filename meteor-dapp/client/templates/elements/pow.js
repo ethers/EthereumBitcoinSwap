b6644f36cd0176f782e151f18b28598c13eb99b5
ethers <ethereum@outlook.com>
2015-07-12 02:46:07 -0700

    prepare for #49 by separating computation with DOM update

--- meteor-dapp/client/templates/elements/pow.js
+++ meteor-dapp/client/templates/elements/pow.js
@@ -8,56 +8,11 @@ Template.pow.viewmodel({
   ticketId: 2,
   nonce: 2460830,
 
+
   findPoWClicked: function() {
-    console.log(this.btcTxHash())
+    var powNonce = computePow(this.btcTxHash(), this.ticketId());
 
-    var hexTicketId = new BigNumber(this.ticketId()).toString(16);
-    var padLen = 16 - hexTicketId.length;
-    var leadZerosForTicketId = Array(padLen + 1).join('0');
-
-    var bnSrc = new BigNumber('0x' + this.btcTxHash() + leadZerosForTicketId + hexTicketId + "0000000000000000");
-    var src;
-    var bnHash;
-    var strHash;
-
-    console.log('@@@ bnSrc: ', bnSrc.toString(16))
-
-
-    src = ku.hexStringToBytes(bnSrc.toString(16));
-    src = new Uint32Array(src.buffer);
-    var srcLen = src.length;
-    var dst = new Uint32Array(8);
-    kecc.digestWords(dst, 0, 8, src, 0, srcLen);
-
-    strHash = ku.wordsToHexString(dst);
-    bnHash = new BigNumber('0x' + strHash);
-
-
-    startTime = new Date().getTime();
-    console.log("startTime: ", startTime)
-
-    var i=0;
-    while (bnHash.gte(bnTarget) && i < 100000000) {
-      bnSrc = bnSrc.add(1);
-
-      src = ku.hexStringToBytes(bnSrc.toString(16));
-      src = new Uint32Array(src.buffer);
-      kecc.digestWords(dst, 0, 8, src, 0, srcLen);
-
-      strHash = ku.wordsToHexString(dst);
-      bnHash = new BigNumber('0x' + strHash);
-
-
-      i+= 1;
-    }
-
-    console.log("endTime: ", new Date().getTime())
-    console.log("duration: ", (new Date().getTime() - startTime) / 1000.0)
-
-    console.log('@@@@ i: ', i)
-    console.log('@@@ strHash: ', strHash)
-
-    this.nonce(i);
+    this.nonce(powNonce);
   },
 
   verifyPoWClicked: function() {
@@ -97,3 +52,56 @@ Template.pow.viewmodel({
     }
   }
 });
+
+
+function computePow(btcTxHash, ticketId) {
+  console.log('@@@ computePow txhash: ', btcTxHash)
+
+  var hexTicketId = new BigNumber(ticketId).toString(16);
+  var padLen = 16 - hexTicketId.length;
+  var leadZerosForTicketId = Array(padLen + 1).join('0');
+
+  var bnSrc = new BigNumber('0x' + btcTxHash + leadZerosForTicketId + hexTicketId + "0000000000000000");
+  var src;
+  var bnHash;
+  var strHash;
+
+  console.log('@@@ bnSrc: ', bnSrc.toString(16))
+
+
+  src = ku.hexStringToBytes(bnSrc.toString(16));
+  src = new Uint32Array(src.buffer);
+  var srcLen = src.length;
+  var dst = new Uint32Array(8);
+  kecc.digestWords(dst, 0, 8, src, 0, srcLen);
+
+  strHash = ku.wordsToHexString(dst);
+  bnHash = new BigNumber('0x' + strHash);
+
+
+  startTime = new Date().getTime();
+  console.log("startTime: ", startTime)
+
+  var i=0;
+  while (bnHash.gte(bnTarget) && i < 100000000) {
+    bnSrc = bnSrc.add(1);
+
+    src = ku.hexStringToBytes(bnSrc.toString(16));
+    src = new Uint32Array(src.buffer);
+    kecc.digestWords(dst, 0, 8, src, 0, srcLen);
+
+    strHash = ku.wordsToHexString(dst);
+    bnHash = new BigNumber('0x' + strHash);
+
+
+    i+= 1;
+  }
+
+  console.log("endTime: ", new Date().getTime())
+  console.log("duration: ", (new Date().getTime() - startTime) / 1000.0)
+
+  console.log('@@@@ i: ', i)
+  console.log('@@@ strHash: ', strHash)
+
+  return i;
+}
