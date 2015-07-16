4cb946f7ad93c970eb818fa0fe804aa1632c0265
ethers <ethereum@outlook.com>
2015-07-16 03:10:56 -0700

    deploy to Olympic using pyepm 24a85bc (ethers/receipt branch) and cpp 4cfc62e

--- meteor-dapp/client/lib/init.js
+++ meteor-dapp/client/lib/init.js
@@ -1,9 +1,10 @@
-web3.setProvider(new web3.providers.HttpProvider('http://localhost:8999'));
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:8549'));
 
 var useBtcTestnet = true;
 var ticketContractAddr;
 if (useBtcTestnet) {
-  ticketContractAddr = '0xc53a82b9b7c9af4801c7d8ea531719e7657aff3c';  // private
+  // ticketContractAddr = '0xc53a82b9b7c9af4801c7d8ea531719e7657aff3c';  // private
+  ticketContractAddr = '0x3e38d51065df2f08dc2ee858a1090c0c56c93bd6'; // Olympic July 16
   // ticketContractAddr = '0x8901a2bbf639bfd21a97004ba4d7ae2bd00b8da8';  // reserveFastExpiry
   // ticketContractAddr = '0x39dfd4315e8b90488ab57e58e4d8b4597a1511e6';  // Olympic
   // ticketContractAddr = '0xb007e8d073af6b6487261bc06660f87ea8740230';
